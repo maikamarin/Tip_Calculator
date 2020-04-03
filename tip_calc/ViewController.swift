85577bf2058de4c68bb1b48274b3acf8384ebf34
maikamarin <56859141+maikamarin@users.noreply.github.com>
2020-04-02 19:29:44 -0700

    Initial Commit

---
+++ tip_calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_calc
+//
+//  Created by Maika Marin on 4/2/20.
+//  Copyright © 2020 Maika Marin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
